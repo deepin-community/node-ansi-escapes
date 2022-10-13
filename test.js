0986d9ea1ed104ff4fa9bbb2576d1a67059ebe2a
Deepin Developer <deepin-dev@deepin.org>
2022-10-13 10:40:43 +0800

    feat: Init commit
    init node-ansi-escapes
    
    Log:

---
+++ test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import ansiEscapes from '.';
+
+test('main', t => {
+	t.true(Object.keys(ansiEscapes).length > 0);
+	t.is(typeof ansiEscapes.cursorTo, 'function');
+	t.is(ansiEscapes.cursorTo(2, 2), '\u001B[3;3H');
+});
